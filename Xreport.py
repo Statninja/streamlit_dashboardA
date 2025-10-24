6ce8eb6e9154ba9b776626750e41617bac6bca2f
silviac <silviacgil@gmail.com>
2025-10-24 11:25:14 +0200

    Update Xreport.py

--- Xreport.py
+++ Xreport.py
@@ -3,8 +3,6 @@ import requests
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
-import plotly.express as px
-import plotly.graph_objects as go
 from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
 import folium
